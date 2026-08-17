c5d512bcf86078a40e803147d9498078b504532a
joseph-merchich <tdury2576@gmail.com>
2026-08-17 15:17:11 -0400

    Update Clothes AI.py

--- Clothes AI.py
+++ Clothes AI.py
@@ -8,21 +8,53 @@ import tensorflow as tf
 from tensorflow.python.framework import ops
 from PIL import Image
 
+# BUG FIX: module is named fashion_mnist (underscore), not fashion.mnist
 fashion_mnist = tf.keras.datasets.fashion_mnist
 
-(train_images, train_labels), (test_images, test_labels) = fashion.mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 class_names = ['T-shirt/top', 'Trouser',
                'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot']
-train_images = train_images / 225.0
-
+train_images = train_images / 255.0
 test_images = test_images / 255.0
 
 model = tf.keras.Sequential()
-
 model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
-model.add(tf.keras.layers.Dense(128, activation='relu' ))
-model.add(tf.keras.layers.Dense(10, activation='softmax' ))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer= tf.optimizers.Adam(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print("10,000 images Test accuracy:", test_acc)import os
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"]="python"
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from PIL import Image
+
+# BUG FIX: module is named fashion_mnist (underscore), not fashion.mnist
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+class_names = ['T-shirt/top', 'Trouser',
+               'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot']
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
 model.compile(optimizer= tf.optimizers.Adam(),
               loss='sparse_categorical_crossentropy',
